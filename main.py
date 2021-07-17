b5c1b183dabf3cc73a1bca95fdfac122264dfebb
ccleary93 <65905077+ccleary93@users.noreply.github.com>
2021-07-17 10:44:34 +0100

    FIX URL BUG, CREATE DICT FOR LOADING
    A bug was found with the URL which was causing the scraper to
    scrape the same ebay results page on a loop. Positioning the &_pgn=
    field in a different part of the URL, and starting the iteration
    from page 2 seems to have fixed this.
    
    Added dictionary creation matching the description to a games 
    console, and creating a new entry with title, console and price 
    fields.

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import time
 import random
 
 
-# START AT PAGE 1 - goes up to page 136. 48 results per page
-pgn = 1
+# START AT PAGE 2 for url to work properly - goes up to page 136. 48 results per page
+pgn = 2
 
 title_list = []
 price_list = []
 
 while pgn < 6:
     # load page number into url, get page data, load into beautifulsoup
-    url = f"https://www.ebay.co.uk/b/Video-Games/139973/bn_450842?rt=nc&LH_Sold=1&mag=1&_sop=13&pgn={str(pgn)}"
+    url = f"https://www.ebay.co.uk/b/Video-Games/139973/bn_450842?LH_Sold=1&mag=1&rt=nc&_pgn={pgn}&_sop=13"
     connection = requests.get(url)
     print(connection)
     ingredients = connection.text
@@ -23,8 +23,22 @@ while pgn < 6:
     # iterate through pages, add delay
     pgn += 1
     time.sleep(random.randint(180,320)/100)
-    
+   
 print(title_list)
 print(price_list)
 print(len(title_list))
 print(len(price_list))
+
+# console list for matching description to console
+consoles = ["PS4", "PLAYSTATION 4", "XBOX ONE", "PLAYSTATION 3","PS3","XBOX 360","PLAYSTATION 2","PS2","GAMECUBE"]
+
+# create dict for loading into database
+load_dict = {}
+for i in range(0,len(title_list)):
+    for console in consoles:
+        if title_list[i].find(console) > 0:
+            load_dict[i] = {"title":title_list[i],
+                            "console":console,
+                            "price":price_list[i]}
+            break
+print(load_dict)
