35e99471942782899ebeff371127a8ba3de44a5b
ccleary93 <65905077+ccleary93@users.noreply.github.com>
2021-07-19 21:11:20 +0100

    Add files via upload

---
+++ data_cleanser.py
@@ -0,0 +1,16 @@
+with open("ps4_games.csv","r",encoding="utf-8") as file:
+    data = file.readlines()
+
+data = [line.rstrip() for line in data]
+print(data)
+punctuation = ["!","(",")","-","[","]","{","}",";",":","'",'"',".","?","&"]
+new_data = []
+for line in data:
+    no_punt = ""
+    for char in line:
+        if char not in punctuation:
+            no_punt += char
+    new_data.append(no_punt+"\n")
+print(new_data)
+with open("ps4_games.csv", "w", encoding="utf-8") as file:
+    file.writelines(new_data)
