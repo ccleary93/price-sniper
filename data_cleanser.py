8a52909f5fff5583ad5a88a5c2f1cf03dc3dd7b5
ccleary93 <65905077+ccleary93@users.noreply.github.com>
2021-08-03 15:10:54 +0100

    Implement multi console search, improve matching
    Implement search for multiples consoles. Improved title matcher - 
    title matcher now takes the longest game title in the console list 
    with a match on the item description.
    
    E.g. 'assassins creed 2' will match only with 'assassins creed 2'
    instead of with 'assassins creed' also.
    
    load_dict now stores the item description and the 'title',
    populated from the game lists. Title field will be used to
    search for past sales data in the database.

--- data_cleanser.py
+++ data_cleanser.py
@@ -2,34 +2,31 @@ class DataCleanser():
     def __init__(self):
         self.punctuation = ["!","(",")","-","[","]","{","}",";",":","'",'"',".","?","&"]
 
+# with open("gamecube_games.csv","r",encoding="utf-8") as file:
+#     data = file.readlines()
+#
+#data = [line.rstrip() for line in data]
+# for line in data:
+#     for char in data:
+#         char.lower()
+# print(data)
+#punctuation = ["!","(",")","-","[","]","{","}",";",":","'",'"',".","?","&"]
+
     def remove_punctuation(self, string):
         no_punct = ""
         for char in string:
             if char not in self.punctuation:
                 no_punct += char
-        return no_punct
+        return no_punct.lower()
 
-
-
-
-
-
-
-##  for removing punctuation from game lists:
-
-# with open("ps4_games.csv","r",encoding="utf-8") as file:
-#     data = file.readlines()
-
-# data = [line.rstrip() for line in data]
-# print(data)
-# punctuation = ["!","(",")","-","[","]","{","}",";",":","'",'"',".","?","&"]
 # new_data = []
 # for line in data:
 #     no_punt = ""
 #     for char in line:
 #         if char not in punctuation:
 #             no_punt += char
-#     new_data.append(no_punt+"\n")
+#     new_data.append(no_punt.lower())
 # print(new_data)
-# with open("ps4_games.csv", "w", encoding="utf-8") as file:
+# with open("gamecube_games.csv", "w", encoding="utf-8") as file:
 #     file.writelines(new_data)
+
