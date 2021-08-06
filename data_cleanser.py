2d5c5f344de322043caf2bec1f164e1f1f2d3177
ccleary93 <65905077+ccleary93@users.noreply.github.com>
2021-08-06 21:44:03 +0100

    Change punctuation blacklist to alphanum whitelist
    Within data cleanser, changed the blacklist of punctuation marks to
    a whitelist of alphanum characters which are allowed - this will eliminate
    all unusual characters.
    
    Applied the new whitelist system to all game lists.

--- data_cleanser.py
+++ data_cleanser.py
@@ -1,10 +1,12 @@
+import string
+
 class DataCleanser():
     def __init__(self):
-        self.punctuation = ["!","(",")","-","[","]","{","}",";",":","'",'"',".","?","&"]
+        self.whitelist = list(" " + string.ascii_lowercase + string.ascii_uppercase + string.digits)
 
     def remove_punctuation(self, string):
-        no_punct = ""
-        for char in string:
-            if char not in self.punctuation:
-                no_punct += char
-        return no_punct.lower()
+        return "".join([char for char in string if char in self.whitelist]).lower()
+        # for char in string:
+        #     if char not in self.punctuation:
+        #         no_punct += char
+        # return no_punct.lower()
