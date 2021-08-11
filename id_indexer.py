271758cc7e4c6bf4480f3ee5209e96ae85e6fce4
ccleary93 <65905077+ccleary93@users.noreply.github.com>
2021-08-11 14:13:47 +0100

    Add error handling; rename attribute
    Added error handling, if the tb is empty the load_last_id
    method returns 0.
    
    Renamed attribute to previous_scrape_start to be more
    logical.

--- id_indexer.py
+++ id_indexer.py
@@ -9,9 +9,12 @@ cursor = db.cursor()
 class ID_Indexer():
     def __init__(self):
         self.tables = ["gamecube", "ps2", "ps3", "ps4", "xbox_360", "xbox_one"]
-        self.id_lister = 0
+        self.previous_scrape_start = 0
 
     def load_last_id(self):
         cursor.execute('''SELECT ebay_id FROM first_item_index WHERE ID = (SELECT MAX(ID) FROM first_item_index);''')
-        self.id_lister = int(str(cursor.fetchall()).split(sep="(")[1].split(sep=",")[0])
-        return self.id_lister
+        try:
+            self.previous_scrape_start = int(str(cursor.fetchall()).split(sep="(")[1].split(sep=",")[0])
+        except IndexError:
+            self.previous_scrape_start = 0
+        return self.previous_scrape_start
